af454b8c74a09b773f761a1f012cff448a882c62
David Morton <davidmorton@xtra.co.nz>
2022-11-09 21:35:11 +1300

    Working commit

--- src/functions_read.cpp
+++ src/functions_read.cpp
@@ -19,6 +19,7 @@ float readEngineElectronicsTemp(Adafruit_MCP9808 t) {
 nissanCanValues nissanCanData;                              // Holds the data to return to caller of function
 int engineTempCelsius;
 int checkEngineLightState;
+bool didWeSeeCheckLightOnStart = 0;
 
 nissanCanValues readNissanDataFromCan(mcp2515_can can) {
     unsigned char len = 0;
@@ -31,19 +32,34 @@ nissanCanValues readNissanDataFromCan(mcp2515_can can) {
 
         // Get the current coolant temperature and engine check light state
         if (canId == 0x551) {
-            nissanCanData.engineTempCelsius = buf[0] - 40;      // Internet info said -48 from hex byte A but does not line up with 
-                                                                // data from NDSIII temperature so -40 it is
+            nissanCanData.engineTempCelsius = buf[0] - 40;
         } else if (canId == 0x180) {
+            // Debug to look at the bits which are set on a particular byte
+            // SERIAL_PORT_MONITOR.println();
+            // for (int i = 7; i >= 0; i--)
+            // {
+            //     bool b = bitRead(buf[7], i);
+            //     SERIAL_PORT_MONITOR.print(b);
+            // }
+
             if (bitRead(buf[7], 4) == 1) {                      // Bit 4 contains check engine light status ie: 0 0 0 1 0 0 0 0
                                                                 // where check light is off when the bit is set
                 nissanCanData.checkEngineLightState = 0;
             }
             if (bitRead(buf[7], 4) == 0) {                      // Bit 4 is not set so check light needs to be set on
                 nissanCanData.checkEngineLightState = 2;
+                didWeSeeCheckLightOnStart = 1;
+            }
+
+            // This is a hacky solution to show the check light on ignition on (providing Arduino has power before ECU)
+            // just so that we can have confidance that the code is working. Debug shows it only sends 2 CAN frames with
+            // the check light bit not set (light on). Unsure how this is catered for in the factory setup.
+            if (millis() < 5000 && didWeSeeCheckLightOnStart == 1) {
+                nissanCanData.checkEngineLightState = 2;
             }
         }
         
-        // This is for debug only to capture all Nissan side CAN data (based on active filters)
+        // Debug to capture all Nissan side CAN data (based on active filters set on the shield)
         // SERIAL_PORT_MONITOR.print("0x");
         // SERIAL_PORT_MONITOR.print(canId, HEX);
         // SERIAL_PORT_MONITOR.print("\t");
@@ -55,4 +71,55 @@ nissanCanValues readNissanDataFromCan(mcp2515_can can) {
         // SERIAL_PORT_MONITOR.println();
     }
     return nissanCanData;
+}
+
+/*****************************************************
+ *
+ * Function - Read latest values from BMW CAN
+ *
+ ****************************************************/
+bmwCanValues bmwCanData;     // Holds the data to return to caller of function
+int vehicleSpeed;
+float wheelSpeedFl = 0;
+float wheelSpeedFr = 0;
+float wheelSpeedRl = 0;
+float wheelSpeedRr = 0;
+
+bmwCanValues readBmwDataFromCan(mcp2515_can can) {
+    unsigned char len = 0;
+    unsigned char buf[8];
+
+    if (CAN_MSGAVAIL == can.checkReceive()) {
+        can.readMsgBuf(&len, buf);
+
+        unsigned long canId = can.getCanId();
+
+        // Get the current vehicle wheel speeds
+        if (canId == 0x1F0) {
+            // (HEX2DEC(B0)+HEX2DEC(RIGHT(B1,1))*256)/16
+            wheelSpeedFl = (buf[0] + (buf[1] >> 4) * 256) / 16;
+            wheelSpeedFr = (buf[2] + (buf[3] >> 4) * 256) / 16;
+            wheelSpeedRl = (buf[4] + (buf[5] >> 4) * 256) / 16;
+            wheelSpeedRr = (buf[6] + (buf[7] >> 4) * 256) / 16;
+        }
+
+        // Calculate the average speed from front wheels and use this as overall vehicle speed
+        vehicleSpeed = (wheelSpeedFl + wheelSpeedFr) /2;
+
+        // Some debug infos
+        SERIAL_PORT_MONITOR.println(wheelSpeedFl);
+        SERIAL_PORT_MONITOR.println(wheelSpeedFr);
+        SERIAL_PORT_MONITOR.println(wheelSpeedRl);
+        SERIAL_PORT_MONITOR.println(wheelSpeedRr);
+        SERIAL_PORT_MONITOR.println();
+
+        // for (int i = 7; i >= 0; i--)
+        // {
+        //     bool b = bitRead(buf[1], i);
+        //     SERIAL_PORT_MONITOR.print(b);
+        // }
+        // SERIAL_PORT_MONITOR.println();
+        // SERIAL_PORT_MONITOR.println();
+    }
+    return bmwCanData;
 }
